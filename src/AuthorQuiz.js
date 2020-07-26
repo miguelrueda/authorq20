17b80d8be35d99346a25430e8b83eb7500c1a5bb
miguel <miguel.rueda@outlook.com>
2020-07-26 15:06:40 -0500

    Module 5 started

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import './bootstrap.min.css';
+import PropTypes from 'prop-types';
 
 function Hero() {
   return (
@@ -13,22 +14,45 @@ function Hero() {
   );
 }
 
-function Turn({author, books}) {
-  console.log(author);
+function Turn({author, books, highlight, onAnswerSelected}) {
+  
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
+    return mapping[highlight];
+  }
+
   return(
-    <div className="row turn" style={{backgroundColor: "white"}}>
+    <div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
       <div className="col-4 offset-1">
         <img src={author.imageUrl} className="authorimage" alt="Author" />
       </div>
       <div className="col-6">
-        {books.map((title) => <Book title={title} key={title}></Book>)}
+        {books.map((title) => <Book title={title} 
+          key={title}
+          onClick={onAnswerSelected}></Book>)}
       </div>
     </div>
   );
 }
 
-function Book({title}) {
-  return (<div className="answer">
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
+};
+
+function Book({title, onClick}) {
+  return (<div className="answer" onClick={() => {onClick(title)}}>
     <h4>{title}</h4>
   </div>);
 }
@@ -49,12 +73,12 @@ function Footer() {
   );
 }
 
-function AuthorQuiz({turnData}) {
+function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
   console.log(turnData);
   return (
     <div className="container-fluid">
       <Hero />
-      <Turn {...turnData} />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
       <Continue />
       <Footer />
     </div>
--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme, { mount, shallow, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { iteratee } from 'underscore';
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
-test('renders learn react link', () => {
-  const { getByText } = render(<AuthorQuiz />);
-  const linkElement = getByText(/author/i);
-  expect(linkElement).toBeInTheDocument();
+const state = {
+  turnData: {
+    books: ["It", 'A Tale of two cities', 'Roughing It'],
+    author:  {
+      name: 'Mark Twain',
+      imageUrl: 'images/authors/marktwain.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['The Adventures of Huckleberry Finn', "Life on the missisippi", "Roughing It"]
+    },
+  },
+  highlight: 'none'
+}
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<AuthorQuiz />);
+//   const linkElement = getByText(/author/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("AUthor Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => {}} />, div)
+  })
 });
