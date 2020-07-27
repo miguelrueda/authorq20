3e922dbb40fb3c0c5106b254837fb353ae922478
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-07-27 11:19:08 -0500

    Module 5 completed

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
 import Enzyme, { mount, shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { iteratee } from 'underscore';
 Enzyme.configure({
   adapter: new Adapter()
 });
@@ -32,4 +31,57 @@ describe("AUthor Quiz", () => {
     const div = document.createElement("div");
     ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => {}} />, div)
   })
+
+  describe("When no answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => {}}/>);
+    });
+
+    it ('Should have no background color', () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    });
+  });
+
+  describe("When the wrong answer has been selected", () => {
+    let wrapper;
+
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'wrong'}))} onAnswerSelected={() => {}} />);
+    });
+
+    it ('Should have a red background color', () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('red');
+    });
+  });
+
+  describe("When the correct answer has been selected", () => {
+    let wrapper;
+
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'correct'}))} onAnswerSelected={() => {}} />);
+    });
+
+    it ('Should have a green background color', () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('green');
+    });
+  });
+
+  describe("When the first answer is selected", () => {
+    let wrapper;
+    const handleAnswerSelected = jest.fn();
+
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={handleAnswerSelected} />);
+      wrapper.find('.answer').first().simulate('click');
+    });
+
+    it ('onAnswerSelected should be called', () => {
+      expect(handleAnswerSelected).toHaveBeenCalled();
+    });
+
+    it('should receive the shining', () => {
+      expect(handleAnswerSelected).toHaveBeenCalledWith("It");
+    });
+  });
 });
